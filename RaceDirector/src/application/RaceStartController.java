7e746f7fa8f8973a07ab0fb94ae4ee210814f9c6
Lucy <luziano.reyna.jr@gmail.com>
2018-10-20 14:54:12 -0500

    -semi overview will start to organize the class and task duties

---
+++ RaceDirector/src/application/RaceStartController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class RaceStartController {
+	
+	Button enterPilots= new Button();
+	Button loadPilots= new Button();
+	
+	
+	int buttonCounter;
+	
+	public String enterPilots(ActionEvent Event) {
+		
+		String ret="You did something";
+		return ret;
+		
+	}
+	
+
+}
