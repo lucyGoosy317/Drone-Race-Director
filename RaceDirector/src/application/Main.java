2317ad70d801561794a1a0ad188eb0bea26acf0d
Lucy <luziano.reyna.jr@gmail.com>
2018-11-06 21:25:10 -0600

    -updated task inside main

--- RaceDirector/src/application/Main.java
+++ RaceDirector/src/application/Main.java
@@ -5,26 +5,40 @@ package application;
  * @author add your names here
  * 
  * 
- * Task:
- * -Create Score class inside Model(Name:Edgard)
- * -Update pilot with an arrayList of Scores , allowing pliot
- * to have scores for each round which will be updated.
+ * Tasks:
+ * (task will be removed once completed, ill be working top-down till finished, please alert me if you are working on 
+ * anything via chat)
  * 
- * -Write Heat controller , which will update the labels with the pilots, and channels. Uitilize
- * already made model classes.
- * embedded (Name:            )
+ * RoundController:
+ * TODO Write previousRound inside RaceOrganizer, that will return String value of The 
+ * previous round info given from rounds arrayList. if the user tries to go below
+ * the given number of rounds inside RaceOrganizer, produce an alert 
+ * TODO Write updateRoundLabel, which will return the 
+ * integer value of the last round and update the label
+ * TODO Write nextRound inside RaceOrganizer, that will return String value of the next round info given from the rounds arrayList
+ * if the user tries to go further than highest value of rounds, produce an alert
  * 
- * -//TODO Create endScoreController, updated score as needed, create a save method
- * inside raceOrganizer save score for pilot, do not overwrite, file will be reread to collect
- * scores, will be displayed from round to round // Create Score fxml, no skin needed, just link
+ * HeatController:
+ * TODO Write heatInfoUpdate inside RaceOrganizer, that get the first heat from the heats arrayList, populate the grid with the heat's pilot data
+ * TODO Write addScores inside RaceOrganizer, which will take given scores that were entered inside the text values, go through the heat list, and
+ * add the current score to the pilot's score array list.
+ * TODO Write timerStarter inside RaceOrganizer, which will update the value of the label timer inside HeatController.
+ *  
+ * TODO Create addUpScore, which will return the String value of pilots inside the pilots generally arraylist, first add the pilot
+ * score inside the pilots score array list and give the sum to the pilot's value of total. Once that is completed sort the general
+ * array list by score.
+ *
+ *
+ *  RaceOffScoreController
+ * TODO Create RaceOff FXML which will display the pilot general list and their scores
+ * TODO Create RaceOff FXML which will call addUpScore, 
+ * 
+ * EndScoreController
+ * TODO Create endScoreController, Will be used for EndScore FXML, Will have TextView type a that will display the pilots
+ * and their score. Use save method to save the general list to a .csv file// Create Score fxml, no skin needed, just link
  * (Name:   Edgard Borrego  )
- *  
- *  -Update RaceOrganizer with an addUpScore Method, method will itirate through a pilots
- *  score arrayList and add the values together, result   will be assigned to a label
- *  
- * -Create raceOffController, conect raceoff  controller componets to
- * a functional level to display data as needed, raceOff fxml (Name:     )
- * 
+ * TODO Create endScore FXML which will show the general list of pilots with pilots total Score to be displayed, sort the
+ * pilots according to score.
  */
 	
 import javafx.application.Application;
