c1f03151731d83b56350612a8ae7d9b8409e5faa
Joe Rodriguez kcf606 <kcf606@my.utsa.edu>
2018-11-27 17:10:54 -0600

    cleaned some stuff up

--- RaceDirector/src/application/controller/RoundController.java
+++ RaceDirector/src/application/controller/RoundController.java
@@ -18,7 +18,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 
 public class RoundController implements Initializable  {
 
