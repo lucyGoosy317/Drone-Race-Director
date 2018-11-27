3bd0aea6b7fc428de958bc986add07b56650a7d3
Joe Rodriguez kcf606 <kcf606@my.utsa.edu>
2018-11-27 17:16:28 -0600

    cleaned up some more stuff

--- RaceDirector/src/application/controller/RaceStartConfigureLoadHeatsController.java
+++ RaceDirector/src/application/controller/RaceStartConfigureLoadHeatsController.java
@@ -18,8 +18,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
 public class RaceStartConfigureLoadHeatsController implements Initializable {
 
