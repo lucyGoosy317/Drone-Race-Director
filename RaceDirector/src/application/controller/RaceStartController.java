3a1baf401cea942be4917bed550c480e204b6776
Joe Rodriguez kcf606 <kcf606@my.utsa.edu>
2018-11-27 17:12:54 -0600

    cleaned up racestartcontroller

--- RaceDirector/src/application/controller/RaceStartController.java
+++ RaceDirector/src/application/controller/RaceStartController.java
@@ -5,9 +5,6 @@ package application.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javax.swing.JOptionPane;
 
 import application.Main;
@@ -16,7 +13,6 @@ import application.model.RaceModelOrganizer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
